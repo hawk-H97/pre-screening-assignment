3947f706a66297101cd0442968c8e2d793db7c40
hawk-H97 <hawk-H@tutanota.com>
2025-04-08 16:02:43 +0530

    update the solution by Singarapalaniappan

--- assignment/backend/database.py
+++ assignment/backend/database.py
@@ -1,8 +1,16 @@
 import os
+import time#
 import psycopg2
-from psycopg2 import sql
+from psycopg2 import OperationalError
 
-DATABASE_URL = os.getenv('# put your databse URL over here to communicate with the Database')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-def get_db_connection():
-    return psycopg2.connect(DATABASE_URL)
+def get_db_connection(retries=5, delay=2):
+    for attempt in range (retries):
+        try:
+            return psycopg2.connect(DATABASE_URL)
+        except OperationalError as e:
+            print(f"Database connection failed (attempt {attempt + 1}/{retries})")
+            print(str(e))
+            time.sleep(delay)
+    raise Exception("Could not connect to the database after serveral retries.")
